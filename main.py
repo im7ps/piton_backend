66768d00a8580aefed74545922eb9c6df4dab00b
emanuele <emanuele.ielo@negg.group>
2023-09-06 17:31:36 +0100

    first deploy

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import auth, admin, users, houses, squares, cities, house_features
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(admin.router)
+app.include_router(users.router)
+app.include_router(auth.router)
+app.include_router(houses.router)
+app.include_router(house_features.router)
+app.include_router(squares.router)
+app.include_router(cities.router)
+
+#Start server uvicorn
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8000)
