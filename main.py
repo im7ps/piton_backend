3104848c56d60f90007b2b8bbd2a3e157f039630
Michele I <mielitro@deloitte.it>
2023-09-07 13:54:22 +0100

    back to first deploy

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, admin, users, houses, squares, cities, house_features
-from cors_middleware import cors_middleware
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.add_middleware(cors_middleware)
+
 
 app.include_router(admin.router)
 app.include_router(users.router)
